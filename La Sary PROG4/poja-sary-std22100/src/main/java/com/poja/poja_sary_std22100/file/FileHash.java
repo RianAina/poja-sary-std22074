52f36d1dbbd7afad8fe2b3cf0318e21e1cfbcc3d
RianAina <128467312+RianAina@users.noreply.github.com>
2024-02-20 10:36:58 +0300

    Poja La Sary Added

---
+++ La Sary PROG4/poja-sary-std22100/src/main/java/com/poja/poja_sary_std22100/file/FileHash.java
@@ -0,0 +1,6 @@
+package com.poja.poja_sary_std22100.file;
+
+import com.poja.poja_sary_std22100.PojaGenerated;
+
+@PojaGenerated
+public record FileHash(FileHashAlgorithm algorithm, String value) {}
---
+++ La Sary PROG4/poja-sary-std22100/src/main/java/com/poja/poja_sary_std22100/file/FileHashAlgorithm.java
@@ -0,0 +1,9 @@
+package com.poja.poja_sary_std22100.file;
+
+import com.poja.poja_sary_std22100.PojaGenerated;
+
+@PojaGenerated
+public enum FileHashAlgorithm {
+  SHA256,
+  NONE
+}
